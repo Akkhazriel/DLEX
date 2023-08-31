ad0d5c33f79b75a38eabda067b21bc8f382f3574
Akkhazriel <akkhazriel@gmail.com>
2023-08-31 14:25:57 +0300

    add header for page 1 with mobile vers

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 import aduptiveMenu from "./modules/menu-button";
+import { dropMenu } from "./modules/menu-button";
--- src/js/modules/menu-button.js
+++ src/js/modules/menu-button.js
@@ -1,14 +1,22 @@
-const burger = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
+const burger = document.querySelector('.nav-menu');
+const menu = document.querySelector('.nav');
 const body = document.body;
 
 
 if (burger && menu) {
     const aduptiveMenu = burger.addEventListener('click', () => {
-        burger.classList.toggle('_active');
-        menu.classList.toggle('_active');
-        body.classList.toggle('no_scroll');
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('no-scroll');
     })
 }
 
 export default aduptiveMenu;
+
+// ON MOBILE
+
+export const dropMenu = 
+document.querySelector('.nav__dropmenu')
+.addEventListener('click', () => {
+    dropMenu.classList.toggle('active');
+})
