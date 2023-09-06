a874832d930b12d5899fc92cad1b933a311b0176
Akkhazriel <akkhazriel@gmail.com>
2023-09-06 10:45:59 +0300

    add section features for page-03

--- src/js/page-02.js
+++ src/js/page-02.js
@@ -7,8 +7,8 @@ window.addEventListener('scroll', () => {
     const logo = document.querySelector('.logo-color');
     
     if (window.scrollY >= triggerELement.offsetTop) {
-            logo.setAttribute("src", "./img/index/header/onDark-logo.svg");
-        } else {
-            logo.setAttribute("src", "./img/page-02/header-02/onDark-logo-purple.svg")
-        }
+        logo.setAttribute("src", "./img/index/header/onDark-logo.svg");
+    } else {
+        logo.setAttribute("src", "./img/page-02/header-02/onDark-logo-purple.svg")
+    }
 });
